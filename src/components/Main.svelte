8755058b771c0e14a0d575e353f6d890b677cd5b
roshan2345 <jha062305@gmail.com>
2023-11-17 19:52:55 +0530

    first commit

---
+++ src/components/Main.svelte
@@ -0,0 +1,263 @@
+<script>
+    import Step from "./Step.svelte";
+
+    let steps = [
+        {
+            name: " OPEN SOURCE CONTRIBUTION",
+            icon: "fa-solid fa-database",
+            description:
+                "A curated list of SurrealDB resources, tools, utilities, and applications.",
+        },
+        {
+            name: "Email sender",
+            icon: "fa-solid fa-envelope",
+            description:
+                "The Email Sender application is a powerful solution crafted with Node.js, Express.js, and MongoDB, designed to seamlessly handle email-related tasks while offering a robust and scalable architecture. This application serves as a comprehensive testing tool, allowing developers to evaluate the performance and reliability of their machine learning models with a focus on email processing and natural language understanding.",
+        },
+        {
+            name: "Ticket app",
+            icon: "fa-solid fa-ticket",
+            description:
+                "The Ticketing Application is a dynamic and feature-rich solution built with Node.js, Express.js, and MongoDB, providing a seamless platform for managing and tracking tickets efficiently. Tailored to meet the demands of various industries, this application offers a robust framework for creating, updating, and monitoring tickets, ensuring a streamlined workflow and optimal user experience.",
+        },
+    ];
+
+    let benefits = [
+        {
+            metric: "10x",
+            name: "Self-Taught Developer with a Foundation in Java",
+            description:
+                "I embarked on my coding journey by delving into Java for data structures and algorithm problem-solving. As my passion for programming grew, I expanded my skill set to include HTML, CSS, and JavaScript, gradually evolving into a well-rounded developer. I've continued to explore various technologies, from frontend frameworks to backend programming, design, and cloud services. My journey is a testament to my love for creativity and problem-solving in the realm of innovative online experiences.",
+        },
+        {
+            name: "Code Enthusiast on a Learning Journey",
+            description:
+               "I am a full-stack developer passionate about coding and technology. While I may be relatively new to the industry, I am enthusiastic about tackling challenges and continuously expanding my skill set. My focus is on creating robust solutions and enhancing user experiences across the digital landscape. Eager to contribute, learn, and grow as a developer.",
+        },
+        {
+            name: "Effective Communicator and Collaborator",
+            description:
+               "While I may lean towards introversion, my commitment to clear and constructive communication remains unwavering. I thrive in fostering transparent dialogues and building strong connections within a team. Despite my introverted nature, I become a people person in the context of work, allowing me to contribute effectively and enhance productivity in any collaborative environment.",
+        },
+    ];
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section
+        id="introPage"
+        class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
+    >
+        <div
+            class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+        >
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi! I'm <span class="poppins text-violet-300">Roshan</span> Jha,
+                <br /> A Full Stack
+                <span class="poppins text-violet-400">Developer</span>
+            </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                My <span class="text-green-400"> favorite tech</span> includes Java(for DSA) and for Dev JavaScript
+                (React.js,NEXT.JS or SvelteKit), TailwindCSS, Node.js + Express.js , PostgreSQL & MongoDB
+                or Firebase/Firestore!
+            </p>
+          <a href="https://twitter.com/roshan_jha_23" target="_blank">
+    <button class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950">
+        <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200" />
+        <h4 class="relative z-9">Get in touch &rarr;</h4>
+    </button>
+</a>
+
+        </div>
+        <div class="relative shadow-2xl grid place-items-center">
+            <img
+                src={"images/profile.png"}
+                alt="Zetane Engine"
+                class="object-cover z-[2] max-h-[70vh]"
+            />
+        </div>
+        <!-- <div  class="flex p-0.5 relative max-w-[700px] w-full mx-auto">
+            <div
+                class="absolute inset-0 overflow-hidden rounded-md flex items-center justify-center"
+            >
+                <div
+                    class="bg-gradient-to-r absolute inset-[-20px]  from-violet-800 to-indigo-800 specialSpin"
+                />
+            </div>
+
+            <img
+                src={"images/zetane-engine.jpeg"}
+                alt="Zetane Engine"
+                class="w-full h-full object-cover z-[2]"
+            />
+        </div> -->
+    </section>
+    <section class="py-20 lg:py-32 flex flex-col gap-24" id="projects">
+        <div class="flex flex-col gap-2 text-center">
+            <h6 class="text-large sm:text-xl md:text-2xl">
+                A few of my endeavors.
+            </h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                Curious to <span class="poppins text-violet-400">see</span> my work?
+            </h3>
+        </div>
+        <a
+            href="https://www.youtube.com/watch?v=Qm-eDEx5qaQ&list=RDQm-eDEx5qaQ&start_radio=1&ab_channel=%D0%95vgenia"
+            target="_blank"
+            class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
+        >
+            <i class="fa-regular fa-circle-play" />
+            <p>Watch the video</p>
+        </a>
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+            <Step step={steps[0]}>
+                <p>
+                   This contri was made to SurrealDB <strong
+                        class="text-violet-400"
+                        >React.js, SurrealDB & Node.js + Express.js!</strong
+                    > A curated list of SurrealDB resources, tools, utilities, and applications.
+                </p>
+            </Step>
+            <Step step={steps[1]}>
+                <p>
+                    Email Sender  is a Backend Application <strong
+                        class="text-violet-400">MongoDB</strong
+                    >,
+                    <strong class="text-violet-400">Node.js + Express.js</strong
+                    >
+                    & <strong class="text-violet-400">Firebase</strong> 
+                    that allows a user to authenticate and send Email presnet in database.
+                </p>
+            </Step>
+            <Step step={steps[2]}>
+                <p>
+                    The Ticket app is a <strong class="text-violet-400"
+                        >React & TailwindCSS</strong
+                    >
+                    web application, hosted on
+                    <strong class="text-violet-400">Netlify</strong>, Whether utilized for customer support, IT helpdesk, or project management, the Ticketing Application stands as a versatile and scalable solution, leveraging the strengths of Node.js, Express.js, and MongoDB to deliver a reliable and user-friendly platform for streamlined ticket management.
+                </p>
+            </Step>
+            <div>
+           <strong> <h1>And More To Come.....</h1> </strong>
+           </div>
+        </div>
+    </section>
+    <section
+        id="about"
+        class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
+    >
+        <!-- <div class="z-[-1] bg-violet-950 w-screen left-1/2 -translate-x-1/2 top-0 h-full absolute"> </div> -->
+        <div
+            class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+        >
+            <h6 class="text-large sm:text-xl md:text-2xl">
+                Want to know more?
+            </h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                A bit <span class="poppins text-violet-400">about</span> me.
+            </h3>
+        </div>
+        <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">
+            I am . . .
+        </p>
+        <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+            {#each benefits as benefit, index}
+                <!-- <div class="flex flex-col gap-2 mx-auto">
+                    <div class="flex items-end gap-4">
+                        <p
+                            class="poppins text-6xl sm:text-7xl md:text-8xl text-slate-500 font-medium"
+                        >
+                            {benefit.metric}
+                        </p>
+                        <p
+                            class="text-xl sm:text-2xl md:text-3xl capitalize pb-2"
+                        >
+                            {benefit.name}
+                        </p>
+                    </div>
+                    <p class="text-center italic">- {benefit.description}</p>
+                </div> -->
+                <div class="flex gap-6 sm:gap-8">
+                    <p
+                        class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
+                    >
+                        0{index + 1}
+                    </p>
+                    <div class="flex flex-col gap-6 sm:gap-8">
+                        <h3 class="text-2xl sm:text-3xl md:text-5xl">
+                            {benefit.name}
+                        </h3>
+                        <p>{benefit.description}</p>
+                    </div>
+                </div>
+            {/each}
+        </div>
+        <h5 class={" text-2xl sm:text-3xl font-semibold text-center poppins "}>
+            The <span class="text-violet-400">Complete</span> Package
+        </h5>
+        <div
+            class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full"
+        >
+            <table class="bg-white text-slate-700 rounded text-center">
+                <thead class={"border-b border-solid border-slate-200  "}>
+                    <tr class="">
+                        <th />
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
+                        <th
+                            class="bg-violet-700 text-white whitespace-nowrap p-4 px-8"
+                            >Me</th
+                        >
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td
+                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                            >Dedication</td
+                        >
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-green-500" /></td>
+                    </tr>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td
+                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                            >Critical Thought</td
+                        >
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-green-500" /></td>
+                    </tr>
+                    <tr>
+                        <td
+                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                            >Interpersonal Skills</td
+                        >
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-green-500" /></td>
+                    </tr>
+                    <tr class="border-t border-solid border-slate-200">
+                        <td
+                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                            >Progamming Ability</td
+                        >
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-green-500" /></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="mx-auto -mt-12 italic sm:hidden opacity-50">
+            <p>Scroll to see more &rarr;</p>
+        </div>
+        <p class="mx-auto">So why not invest?</p>
+    </section>
+</main>
